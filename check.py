5fed6e1ee3bac269613dd844ed6810e661a50de5
Seth Troisi <sethtroisi@google.com>
2020-10-08 10:52:55 -0700

    Allow '?' in primecert column

--- check.py
+++ check.py
@@ -85,7 +85,7 @@ def check():
         lines = f.readlines()
 
     LINE_RE = re.compile(
-        r"INSERT INTO gaps VALUES.(\d+),[01],'C','[F?]','[CP]','[\w.&]*',"
+        r"INSERT INTO gaps VALUES.(\d+),[01],'C','[F?]','[CP?]','[\w.&]*',"
         r"(-300|[12][890]\d\d),"
         r"([0-9.]+),([0-9]+),'([^']+)'\);")
 
