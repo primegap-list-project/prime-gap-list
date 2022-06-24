cd0eef18364775e1c23b65ae7eab4eab22d957f2
Seth Troisi <sethtroisi@google.com>
2022-06-24 15:40:01 -0700

    Changed primecret to gapcert

--- schema.sql
+++ schema.sql
@@ -1,6 +1,6 @@
 PRAGMA foreign_keys=OFF;
 BEGIN TRANSACTION;
-CREATE TABLE IF NOT EXISTS gaps (gapsize INTEGER,ismax BOOLEAN,primecat TEXT,isfirst TEXT,primecert TEXT,discoverer TEXT,year INTEGER,merit REAL,primedigits INTEGER,startprime BLOB);
+CREATE TABLE IF NOT EXISTS gaps (gapsize INTEGER,ismax BOOLEAN,primecat TEXT,isfirst TEXT,gapcert TEXT,discoverer TEXT,year INTEGER,merit REAL,primedigits INTEGER,startprime BLOB);
 CREATE TABLE IF NOT EXISTS credits (abbreviation TEXT,name TEXT,ack TEXT,display TEXT);
 CREATE INDEX IF NOT EXISTS gaps_gapsize ON gaps (gapsize);
 COMMIT;
